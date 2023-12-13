511eb47e619544c7bf0d48f7c7798cee07cabf79
Josiah Underwood <josiahunderwood88@gmail.com>
2023-12-13 15:25:20 -0800

    added password reset unit tests

--- Test/UnitTests/PaswordResetUnitTests.cs
+++ Test/UnitTests/PaswordResetUnitTests.cs
@@ -4,6 +4,7 @@ using Data.DTO;
 using Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Microsoft.Win32;
 using Moq;
 using Persistence;
@@ -19,14 +20,12 @@ namespace Test.UnitTests {
 
         public PaswordResetUnitTests() {
             var options = new DbContextOptionsBuilder<DataContext>()
-              .UseInMemoryDatabase(databaseName: "TestAuthDb")
-              .EnableSensitiveDataLogging()
-               .Options;
-
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
 
             var inMemorySettings = new Dictionary<string, string>
              {
-                 {"JWT_SECRET", "zivmxwYN+TzH+oOl7pqB1jvRjnwjAJG5NKMJXp8Of8WU4SrxX+o9ykMWWI7cyuHG\ngPOvvHT0gVvajO7+4OX+Jg=="},
+                 {"JWT_SECRET_KEY", "zivmxwYN+TzH+oOl7pqB1jvRjnwjAJG5NKMJXp8Of8WU4SrxX+o9ykMWWI7cyuHG\ngPOvvHT0gVvajO7+4OX+Jg=="},
 
             };
 
@@ -34,14 +33,11 @@ namespace Test.UnitTests {
             .AddInMemoryCollection(inMemorySettings)
             .Build();
 
-
             _tokenService = new TokenService(_configuration);
 
             _db = new DataContext(options);
 
-
             _mockEmailService = new Mock<IEmailService>();
-
 
             _authService = new AuthService(_db, _tokenService, _mockEmailService.Object);
         }
@@ -62,6 +58,144 @@ namespace Test.UnitTests {
             Assert.Equal(user.AppUserId, resetRequest.AppUserId);
             Assert.False(resetRequest.IsExpiredOrFailed);
             Assert.False(resetRequest.UserHasValidated);
+        }
+
+        [Fact]
+        public async Task Successful_ValidateResetCodeAsync_Attempt() {
+            var user = await CreateUser();
+            var passwordReset = await CreatePasswordResetEntity(user.AppUserId);
+
+            var validateRequest = new ResetCodeConfirmation {
+                Email = user.Email,
+                Code = passwordReset.Code
+            };
+
+            await _authService.ValidateResetCodeAsync(validateRequest);
+
+            var res = await _db.PasswordResetRequests.FirstOrDefaultAsync();
+
+            Assert.NotNull(res);
+            Assert.True(res.UserHasValidated);
+
+        }
+
+        [Fact]
+        public async Task Successful_ResetPasswordAysnc_Attempt() {
+            var user = await CreateUser();
+            var passwordReset = await CreatePasswordResetEntity(user.AppUserId);
+
+            var validateRequest = new ResetCodeConfirmation {
+                Email = user.Email,
+                Code = passwordReset.Code
+            };
+
+            await _authService.ValidateResetCodeAsync(validateRequest);
+
+            var reset = new ResetPasswordDto {
+                Email = user.Email,
+                Password = "StrongPassword1@",
+                ConfirmPassword = "StrongPassword1@"
+            };
+
+            var resetRes = await _authService.ResetPasswordAysnc(reset);
+            var authRes = await _db.Authentication.FirstOrDefaultAsync();
+
+            Assert.NotNull(authRes);
+            Assert.NotNull(resetRes);
+            Assert.NotEmpty(resetRes);
+            Assert.Equal(2, authRes.LoginCount);
+
+        }
+
+        [Fact]
+        public async Task ValidDateResetCode_Exceeds_Attempts() {
+            var user = await CreateUser();
+            await CreatePasswordResetEntity(user.AppUserId);
+
+            var validateRequest = new ResetCodeConfirmation {
+                Email = user.Email,
+                Code = 9999
+            };
+
+            int validationAttempts = 4;
+
+            for (int i = 0; i < validationAttempts; i++) {
+
+                try {
+                    await _authService.ValidateResetCodeAsync(validateRequest);
+                }
+                catch (Exception ex) {
+                    Assert.NotEmpty(ex.Message);
+                }
+            }
+
+            var passwordRes = _db.PasswordResetRequests.FirstOrDefault();
+
+            Assert.NotNull(passwordRes);
+            Assert.Equal(3, passwordRes.CodeEntryCount);
+            Assert.True(passwordRes.IsExpiredOrFailed);
+            Assert.False(passwordRes.UserHasValidated);
+
+        }
+
+        [Fact]
+        public async Task ValidateResetCode_Exceeds_Time_Limit() {
+            var user = await CreateUser();
+            var resetRequest = await CreatePasswordResetEntity(user.AppUserId);
+
+            var backDatedTime = DateTime.UtcNow.AddMinutes(-11);
+
+            resetRequest.SendDate = backDatedTime;
+            _db.Update(resetRequest);
+            await _db.SaveChangesAsync();
+
+            var validateRequest = new ResetCodeConfirmation {
+                Email = user.Email,
+                Code = 1111
+            };
+            try {
+                await _authService.ValidateResetCodeAsync(validateRequest);
+            }
+            catch (Exception ex) {
+                Assert.NotEmpty(ex.Message);
+                Assert.Equal("Reset code expired, please request new code", ex.Message);
+            }
+
+        }
+
+        [Fact]
+        public async Task Unsuccessful_RequestPassword_Reset() {
+
+            try {
+
+                await _authService.SendResetEmailAsync("someemail@test.com");
+
+            }
+            catch (Exception ex) {
+
+                Assert.NotNull(ex);
+                Assert.Equal("Email does not exits", ex.Message);
+
+            }
+
+        }
+
+        private async Task<PasswordResetRequest> CreatePasswordResetEntity(Guid userId) {
+            var passwordReset = new PasswordResetRequest {
+                PasswordRequestId = Guid.NewGuid(),
+                AppUserId = userId,
+                Code = 1111,
+                SendDate = DateTime.UtcNow,
+                UserHasValidated = false,
+                CodeEntryCount = 0,
+                IsExpiredOrFailed = false
+
+            };
+
+            _db.Add(passwordReset);
+            await _db.SaveChangesAsync();
+            return passwordReset;
+
         }
 
         private async Task<AppUser> CreateUser() {
